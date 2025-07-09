c59fa6ea6140873f2158b858e6e79a8a39701c6b
xucheng <3020271276@qq.com>
2025-07-09 15:17:12 +0800

    新增Sample(样例)实体

---
+++ src/main/java/com/github/cnxucheng/xcoj/model/entity/Sample.java
@@ -0,0 +1,14 @@
+package com.github.cnxucheng.xcoj.model.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Sample implements Serializable {
+
+    private String input;
+
+    private String output;
+
+}
